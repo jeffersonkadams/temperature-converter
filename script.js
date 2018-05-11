04b1be264657a018d44cf13c379fa21edaacf89a
Jeff <jeffersonkadams@gmail.com>
2018-05-10 21:29:27 -0400

    all functionality done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("loaded");
+
+function convertTemp(originalTemp){
+  var originalTemp = document.getElementById('input').value; // number of the original temperature
+  var newTemp; //number of the new temperature
+  console.log(originalTemp);
+
+  if (document.getElementById("checkC").checked == true){
+    console.log("Celcius checked");
+    newTemp = (originalTemp * (9/5)) + 32;
+  }else {
+    newTemp = (originalTemp - 32) * (5/9);
+  }
+
+  console.log(newTemp);
+  document.getElementById('output').innerHTML = newTemp;
+}
