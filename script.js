31fceb4883ec7dad81b960abf6a1e017761d7eae
Jeff <jeffersonkadams@gmail.com>
2018-05-11 16:13:21 -0400

    cleaned up the output before styling.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function convertTemp(originalTemp){
   var originalTemp = document.getElementById('input').value; // number of the original temperature
   var newTemp; //number of the new temperature
+  var tempType = document.getElementById('tempTypeInput').value;
+  var convertedType;
 
-  if (document.getElementById("checkC").checked == true){
+  if (document.getElementById("tempTypeInput").value === "Celcius"){
     newTemp = (originalTemp * (9/5)) + 32;
+    convertedType = 'Fahrenheit';
   }else {
     newTemp = (originalTemp - 32) * (5/9);
+    convertedType = 'Celcius';
   }
-  document.getElementById('output').innerHTML = newTemp;
+  document.getElementById('output').innerHTML = originalTemp + " degrees " + tempType + " is " + newTemp.toFixed(2) + " degrees " + convertedType;
 }
